e5c07650f2861384b527b8fd97d405e6aa757f9a
Rynhardt Cloete <rynhardtcloete@gmail.com>
2016-12-24 20:23:27 +0200

    Conky files edited

--- Conky wifi/conkyStart.sh
+++ Conky wifi/conkyStart.sh
@@ -1,17 +1,1 @@
-#rm $HOME/.config/autostart/conky.desktop
-
-#printf "[Desktop Entry]\n\
-#Type=Application\n\
-# [Desktop Entry]
-#
-# Type=Application
-#
-# Exec=/~/github/configs/Conky\ wifi/conkyStart.sh
-#sleep 25
 /usr/bin/conky -c $HOME/.conky/conky &
-#X-GNOME-Autostart-enabled=true\n\
-#NoDisplay=false\n\
-#Hidden=false\n\
-#Name[en_ZA]=conky\n\
-#Comment[en_ZA]=Conky Wifi\n\
-#X-GNOME-Autostart-Delay=5\n" > $HOME/.config/autostart/conky.desktop
